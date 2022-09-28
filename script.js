1716d170c0c7718c46ee9084d8743d17c11eba78
Hahdrim S <romain.bussieres@live.fr>
2022-09-28 16:43:32 +0200

    second version

--- script.js
+++ script.js
@@ -1,27 +1,63 @@
 // get button from DOM
 startButton = document.getElementById('start')
 
-//Choose a random word from a "words" list when player hits button
+
+let words = ["plume", "grimoir", "pierre", "dragon", "bouclier", "corbeau", "tour", "chevalier"]
+let secretBoxes = document.getElementsByClassName('secret-word')
+let displayedTimer = document.getElementById('time')
+
 
 const start = ()=>{
-let words = ["plume", "grimoir", "pierre", "dragon", "bouclier", "corbeau", "tour", "chevalier"]
-let wordNumber = Math.floor(Math.random()*(words.length))
-let selectedWord = words[wordNumber]
-console.log(selectedWord)
 
-// Select a random location on the interface in which the word will appear
-const selectDiv = ()=>{
-    let secretBoxes = document.getElementsByClassName('secret-word')
-let wordLocation = Math.floor(Math.random()*(secretBoxes.length))
-let selectedLocation = secretBoxes[wordLocation]
+    // start timer
+    const timer = () =>{
+        if (displayedTimer.innerHTML == 120){
+            var sec = 120;
+            var timer = setInterval(function(){
+            displayedTimer.innerHTML=sec;
+            sec--;
+            if (sec < 0) {
+                clearInterval(timer);
+            }
+        }, 1000); 
+        }
+    }
+    timer()
 
-console.log(selectedLocation)
+    // check if boxes are empty
 
-selectedLocation.innerHTML += `selectedWord`
+    let allBoxesEmpty = true
+
+    for (box of secretBoxes){
+        if (box.innerHTML != ""){
+            allBoxesEmpty = false
+        }
+    }
+
+    if (allBoxesEmpty) {
+
+    //Choose a random word from a "words" list
+    let wordNumber = Math.floor(Math.random()*(words.length))
+    let selectedWord = words[wordNumber]
+
+    // Select a random location on the interface in which the word will appear
+    let wordLocation = Math.floor(Math.random()*(secretBoxes.length))
+    let selectedLocation = secretBoxes[wordLocation]
+
+    // Insert selected word in selected location
+    selectedLocation.innerHTML += selectedWord 
+    
+    console.log(wordLocation)
+    }
+
+    else {
+        console.log('Une partie est déjà en cours !')
+    }
 }
+
+
+// start game when player hits button
+
 startButton.addEventListener("click", () => {
     start()
 })
-
-
-// TODO : set timer / unite functions
