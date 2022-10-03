7a0b752097956a253db343c0ca4805a7c7e92fb8
Romain Bussieres <romain.bussieres@intranet.croix-rouge.asso.fr>
2022-10-03 13:35:45 +0200

    need to fix submit button for users answer

--- script.js
+++ script.js
@@ -3,51 +3,61 @@ startButton = document.getElementById('start')
 
 
 let words = ["plume", "grimoir", "pierre", "dragon", "bouclier", "corbeau", "tour", "chevalier"]
+let selectedWord = ""
 let secretBoxes = document.getElementsByClassName('secret-word')
 let displayedTimer = document.getElementById('time')
+let playersAnswer = document.getElementById('players-answer').value
+let sumbitAnswerButton = document.getElementById('submit-answer')
 
+const displayAnswer = () =>{
+    console.log(playersAnswer)
+}
 
-const start = ()=>{
+sumbitAnswerButton.addEventListener('submit', function(e) {
+    e.preventDefault();
+    displayAnswer();
+});
+
+function start() {
 
     // start timer
-    const timer = () =>{
-        if (displayedTimer.innerHTML == 120){
-            var sec = 120;
-            var timer = setInterval(function(){
-            displayedTimer.innerHTML=sec;
-            sec--;
-            if (sec < 0) {
-                clearInterval(timer);
-            }
-        }, 1000); 
+    const timer = () => {
+        if (displayedTimer.innerHTML == 120) {
+            var sec = 120
+            var timer = setInterval(function () {
+                displayedTimer.innerHTML = sec
+                sec--
+                if (sec < 0) {
+                    clearInterval(timer) // Change that line to end the game at the end of the timer (will need a new function or a variable gameOn set to False when timer ends)
+                }
+            }, 1000)
         }
     }
     timer()
 
     // check if boxes are empty
-
     let allBoxesEmpty = true
 
-    for (box of secretBoxes){
-        if (box.innerHTML != ""){
+    for (box of secretBoxes) {
+        if (box.innerHTML != "") {
             allBoxesEmpty = false
         }
     }
 
     if (allBoxesEmpty) {
 
-    //Choose a random word from a "words" list
-    let wordNumber = Math.floor(Math.random()*(words.length))
-    let selectedWord = words[wordNumber]
+        //Choose a random word from a "words" list
+        let wordNumber = Math.floor(Math.random() * (words.length))
+        let selectedWord = words[wordNumber]
 
-    // Select a random location on the interface in which the word will appear
-    let wordLocation = Math.floor(Math.random()*(secretBoxes.length))
-    let selectedLocation = secretBoxes[wordLocation]
+        // Select a random location on the interface in which the word will appear
+        let wordLocation = Math.floor(Math.random() * (secretBoxes.length))
+        let selectedLocation = secretBoxes[wordLocation]
 
-    // Insert selected word in selected location
-    selectedLocation.innerHTML += selectedWord 
-    
-    console.log(wordLocation)
+        // Insert selected word in selected location
+        selectedLocation.innerHTML += selectedWord
+
+        console.log(wordLocation)
     }
 
     else {
