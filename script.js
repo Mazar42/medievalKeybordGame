d6b278d3909e88477728c076729ecdfbd68db5a4
Hahdrim S <romain.bussieres@live.fr>
2022-10-10 12:15:34 +0200

    point count ok, answer validation ok, need to set the loop according to timer

--- script.js
+++ script.js
@@ -2,34 +2,14 @@
 startButton = document.getElementById('start')
 
 let words = ["plume", "grimoir", "pierre", "dragon", "bouclier", "corbeau", "tour", "chevalier"]
-let selectedWord = ""
 let secretBoxes = document.getElementsByClassName('secret-word')
 let displayedTimer = document.getElementById('time')
 let sumbitButton = document.getElementById('submit-answer')
-let playersanswer = ""
-
-
+let currentScore = document.getElementById('score')
 
 function start() {
-
-    // start timer
-    const timer = () => {
-        if (displayedTimer.innerHTML == 120) {
-            var sec = 120
-            var timer = setInterval(function () {
-                displayedTimer.innerHTML = sec
-                sec--
-                if (sec < 0) {
-                    clearInterval(timer) // Change that line to end the game at the end of the timer (will need a new function or a variable gameOn set to False when timer ends)
-                }
-            }, 1000)
-        }
-    }
-    timer()
-
     // check if boxes are empty
     let allBoxesEmpty = true
-
     for (box of secretBoxes) {
         if (box.innerHTML != "") {
             allBoxesEmpty = false
@@ -47,33 +27,57 @@ function start() {
         let selectedLocation = secretBoxes[wordLocation]
 
         // Insert selected word in selected location
-        selectedLocation.innerHTML += selectedWord
+        selectedLocation.innerHTML = selectedWord
 
+        console.log(selectedWord)
+        
+        var points = 0
+        let currentScore = document.getElementById('score')
+        
         function getInputValue() {
             // Selecting the input element and get its value 
         let playersanswer = document.getElementById("inputId").value;
             // Displaying the value
+            console.log(selectedWord + "" + playersanswer)
            if(playersanswer == selectedWord){
-            console.log('bien joué frérot')
+            points ++
+            console.log(points)
+            currentScore.innerHTML = points
            }
            else{
-            console.log("ben non t'es nul là frérot")
+            alert("Oups ! Ce n'est pas le bon mot... 😞")
            }
-          }
+        }
 
         sumbitButton.addEventListener('click', () => {
             getInputValue()
         })
+        
     }
 
     else {
         alert('Une partie est déjà en cours !')
     }
+
+    // start timer
+    const timer = () => {
+        if (displayedTimer.innerHTML == 45) {
+            var sec = 45
+            var timer = setInterval(function () {
+                displayedTimer.innerHTML = sec
+                sec--
+                if (sec < -1) {
+                    clearInterval(timer)
+                    if(!alert("Fin de la partie ! Vous avez trouvé " + points + " mot(s) !")){window.location.reload();} 
+                }
+            }, 1000)
+        }
+    }
+    timer()
     
 }
 
 // start game when player hits button
-
 startButton.addEventListener("click", () => {
     start()
 })
